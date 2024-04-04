5c269b31de155402defb4c9a88ac2903be726a4a
jcwilliams2 <124515633+jcwilliams2@users.noreply.github.com>
2024-04-04 16:59:05 -0400

    Add list of movie genres and create a list of list containing data for different movies

---
+++ movie_data.py
@@ -0,0 +1,9 @@
+genres = ['Romance', 'Horror', 'Drama', 'Animation', 'Musical', 'Documentary', 'Action']
+movies = [[['romance'], "The Notebook", "Nick Cassavetes", 2004],
+          [['horror'], "Halloween", "John Carpenter", 1978],
+          [['animation', 'family'], "Spirited Away", "Hayao Miyazaki", 2001],
+          [['animation', 'family'], "Zootopia", "Byron Howard & Rich Moore", 2016],
+          [['action'], "The Beekeeper", "David Ayer", 2024],
+          [['musical'], "The Wiz", "Sidney Lument", 1978],
+          [['documentary'], "Our Father", "Lucie Jourdan", 2022],
+          [['drama', 'comedy'], "The First Wives Club", "Hugh Wilson", 1996]]
